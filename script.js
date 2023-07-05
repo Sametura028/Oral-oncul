b435a7f5b98f18c050fdd6adba9d68034b875f71
Sametura028 <sametucar0028@gmail.com>
2023-07-05 08:23:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("myDropdown");
+    if (dropdownMenu.style.display === "none") {
+      dropdownMenu.style.display = "block";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+  }
+  
