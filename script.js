f0e30e1c821c86c67fc70152f3e0165663312717
Sametura028 <sametucar0028@gmail.com>
2023-07-09 01:18:31 +0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById("myDropdown");
-    if (dropdownMenu.style.display === "none") {
-      dropdownMenu.style.display = "block";
-    } else {
-      dropdownMenu.style.display = "none";
+
+$('.slide').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots:true,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 994,
+      settings: {
+        dots: false
+      }
     }
-  }
-  
+  ]
+});
+$('.resslide').slick({
+  dots:false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 1350,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    }
+
+  ]
+});
